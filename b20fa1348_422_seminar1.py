1de8117aa71506ce08e45e7937e2be39613f9dc3
Unabold <113616615+Unabold@users.noreply.github.com>
2022-09-15 23:33:26 +0800

    DBP

---
+++ b20fa1348_422_seminar1.py
@@ -0,0 +1,130 @@
+#1
+#lists = ['python','php','java']
+#print(lists[0:3])
+
+#2
+#num = [1,2,3,4,5,6,7,8,9,10]
+#c=0 
+#for i in num:
+  #      c=c+i
+ #       i=i+1
+#print(c)
+
+#3
+#num = [10,27,8,19,12]
+#def multiplier(x):
+    #c=1
+   #      c=c*i
+ #       i=i+1
+#    print(c)
+#multiplier(num)
+#4
+#def multiply():
+#    numbers= list(map(int,input('').split(',')))
+#    multiplier = numbers[2]*numbers[-1]
+#    print(multiplier)
+#multiply()
+
+#5
+#def func():
+    #numbers= list(map(int,input('').split(',')))
+#a = max(numbers)
+#b = min(numbers)
+
+#6
+#a = ['abdba', 'abcd', '121']
+#hariu = 0
+#for i in a:
+#    i = str()
+#    if i[0]==i[-1]:
+#        hariu = hariu + 1
+#print(hariu)
+
+#8
+# list = []
+# def func(list):
+#     if len(list) == 0:
+#         print('List is empty')  
+#     else:
+#         print('List is not empty')
+# func(list)
+
+#9
+# alist = [1,2,6,31,70,8,411,5,900,12]
+# del alist[4], alist[6], alist[8]
+# print(alist)
+
+#10,11
+# numbers = (1,2,3,4,5,6,7,8,9,10)
+# y = list(numbers)
+# y.append(input())
+# numbers = tuple(y)
+# print(numbers)
+
+#12
+# numbers = (1,2,3,4,5,6,7,8,9,10)
+# print(numbers[1],numbers[-2])
+
+
+#14
+# numbers = (1,2,3,4,5,6,7,8,9,10)
+# k = 0
+# for i in numbers:
+#     print(i)
+#     i += 1
+
+#15 
+#friuts = {'apple','banana','orange'}
+#friuts2 = {'grape','strawberry'}
+#newset = friuts.union(friuts2)
+#print(newset)
+
+#16
+#friuts = {'apple','banana','orange'}
+#friuts2 = {'grape','strawberry', 'orange'}
+#newset = friuts.intersection(friuts2)
+#print(newset)
+
+#17
+#friuts = {'apple','banana','orange'}
+#print(len(friuts))
+
+#18
+# x = input()
+# friuts = {'apple','banana','orange'}
+# print(friuts)
+#friuts.remove(y)
+# print(friuts)
+
+#19
+#friuts = {'apple','banana','orange'}
+#friuts.clear()
+
+
+#20
+#friuts = {'apple','banana','orange'}
+#del friuts
+
+#22
+#num = {'Manufacturer': 'Tesla','Model': 'X', 'Year': '2001'}
+#if 'year' in num:
+#    print('Yes')
+#else:
+#    print('No')
+
+#23
+# num = {'Manufacturer': 'Tesla','Model': 'X', 'Year': '2001'}
+# if '2001' in num.values():
+#     print('Yes')
+# else:
+#     print('No')
+
+#24
+# num = {'Manufacturer': 'Tesla','Model': 'X', 'Year': '2001'}
+# for i,k in num.items():
+#     print(i,k)
+
+#26
+#num = {'Manufacturer': 'Tesla','Model': 'X', 'Year': '2001'}
+#print(sum(num.values()))
+
